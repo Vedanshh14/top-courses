1caba0454e31d84320614f79a8668176b5f62b46
Vedanshh14 <vedanshupadhyay1401@gmail.com>
2025-02-22 21:33:00 +0530

    project completed

--- src/Components/Filter.jsx
+++ src/Components/Filter.jsx
@@ -6,8 +6,8 @@ const Filter = ({ filterData, category, setCategory }) => {
       {filterData.map((item) => (
         <button
           key={item.id}
-          className={`px-4 py-2 rounded-md text-black bg-slate-200 
-            ${category === item.title ? "border-2 border-white" : "border-none"}`}
+          className={`px-4 py-2 rounded-md text-white bg-slate-600 
+            ${category === item.title ? "border-2 border-black" : "border-none"}`}
           onClick={() => setCategory(item.title)}
         >
           {item.title}
